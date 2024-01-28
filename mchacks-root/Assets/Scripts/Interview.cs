c2d07d200b454e61ad01b039a67a5bb73d9a1884
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-01-27 22:06:19 -0500

    feat: new question canvas

---
+++ mchacks-root/Assets/Scripts/Interview.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Interview : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI questionText;
+    
+}
